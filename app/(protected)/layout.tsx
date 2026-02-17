e163359605cb56fe2ec67de4eb641e3dc59c8724
Marcin <kowalczyk.marcin1997@gmail.com>
2026-02-17 11:18:51 +0100

    fix - keyboard view scroll problem

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -17,7 +17,7 @@ const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
       <Toaster position="top-center" richColors />
       <SidebarProvider defaultOpen={true}>
         <AppSidebar />
-        <SidebarInset className="flex-1 flex flex-col h-svh w-full md:w-[calc(100%-var(--sidebar-width-expanded))] md:peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-collapsed))]">
+        <SidebarInset className="flex-1 flex flex-col h-dvh w-full md:w-[calc(100%-var(--sidebar-width-expanded))] md:peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-collapsed))]">
           {isMobile ? (
             <header className="flex h-16 bg-sidebar-background border-b border-sidebar-border shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
               <div className="flex justify-between items-center gap-2 px-4">
@@ -27,7 +27,7 @@ const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
               </div>
             </header>
           ) : null}
-          <div className="flex flex-col gap-2 p-3 pb-10 h-full overflow-y-auto overflow-x-hidden bg-color-background">
+          <div className="flex flex-col gap-2 p-3 pb-10 min-h-0 flex-1 overflow-y-auto overflow-x-hidden bg-color-background">
             {children}
           </div>
         </SidebarInset>
