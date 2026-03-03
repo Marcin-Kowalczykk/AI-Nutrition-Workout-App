284daa32da736dbd04f7885e6d85012aff669aa2
Marcin <kowalczyk.marcin1997@gmail.com>
2026-03-03 14:06:07 +0100

    Fix required suspense due to deploy/build fail

--- app/(protected)/workout/template/page.tsx
+++ app/(protected)/workout/template/page.tsx
@@ -1,11 +1,12 @@
+import { Suspense } from "react";
 import { TemplateList } from "@/components/workout-template/template-list";
 import { ViewTemplateSheet } from "@/components/workout-template/view-template-sheet";
 
 export default function TemplatesPage() {
   return (
-    <>
+    <Suspense fallback={null}>
       <TemplateList />
       <ViewTemplateSheet />
-    </>
+    </Suspense>
   );
 }
