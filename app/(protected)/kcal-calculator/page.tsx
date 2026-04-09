a3169edc42017406adb15ac3eda0b856aebad274
Marcin <kowalczyk.marcin1997@gmail.com>
2026-04-09 08:25:52 +0200

    Diet panel - wire up KcalCalculatorPage

--- app/(protected)/kcal-calculator/page.tsx
+++ app/(protected)/kcal-calculator/page.tsx
@@ -1,20 +1,5 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { KcalCalculator } from "@/components/kcal-calculator/kcal-calculator";
 
 export default function KcalCalculatorPage() {
-  return (
-    <div className="w-full xl:w-1/2">
-      <Card>
-        <CardHeader>
-          <CardTitle>Kcal calculator</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p className="text-sm text-muted-foreground">
-            This section is in progress. Soon you&apos;ll be able to calculate
-            and manage your daily calorie needs here.
-          </p>
-        </CardContent>
-      </Card>
-    </div>
-  );
+  return <KcalCalculator />;
 }
-
