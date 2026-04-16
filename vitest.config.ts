5787f3bfc1ff29c4ae65f2e5b6c5a5b0e2fbc856
Marcin <kowalczyk.marcin1997@gmail.com>
2026-04-17 00:49:23 +0200

    workout panel - refactor - exclude .worktrees from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./tests/setup.ts'],
-    exclude: ['**/node_modules/**', '**/e2e/**'],
+    exclude: ['**/node_modules/**', '**/e2e/**', '**/.worktrees/**'],
     coverage: {
       provider: 'v8',
       reporter: ['text', 'html'],
